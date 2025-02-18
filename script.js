d0632c5f32029b04c1ddb030800ab3d774abbf89
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2025-02-18 19:22:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 //complete this code
-class Person {}
+class Person {
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
 
-class Student extends Person {}
+    getname() {
+        return this.name;
+    }
 
-class Teacher extends Person {}
+    setAge(newAge){
+        this.age = newAge;
+    }
+}
+
+class Student extends Person {
+    study(){
+        // super(this.name, age);
+        console.log(`${this.name} is studying`)
+    }
+}
+
+class Teacher extends Person {
+    teach(){
+        // super(this.name, age);
+        console.log(`${this.name} is teaching`)
+    }
+}
 
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
+
+
